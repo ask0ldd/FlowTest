c2de850784d22d56e4816dd0d78edd0fd69c2dbd
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-13 10:23:17 +0100

    *edit* front / add node

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { ReactFlow } from '@xyflow/react';
+import { Background, BackgroundVariant, ReactFlow } from '@xyflow/react';
  
 import '@xyflow/react/dist/style.css';
+import { useState } from 'react';
  
 const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
@@ -9,9 +10,25 @@ const initialNodes = [
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
  
 export default function App() {
+
+  const [nodes, setNodes] = useState(initialNodes)
+
+
+  function handleAddNode(event: React.MouseEvent<HTMLSpanElement, MouseEvent>): void {
+    event.preventDefault()
+    setNodes(prevNodes => [...prevNodes, {id:'3', position : {x:0, y:200}, data : {label : '3'}}])
+  }
+
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    <div style={{display:'flex', width: '100vw', height: '100vh'}}>
+      <div style={{ width: '80%', height: '100%' }}>
+        <ReactFlow nodes={nodes} edges={initialEdges} >
+          <Background color="#333" variant={BackgroundVariant.Dots} />
+        </ReactFlow>
+      </div>
+      <div style={{display:'flex', flexDirection:'column', width:'20%', height:'100%', background:'#EFEFEF'}}>
+        <span onClick={handleAddNode}>add node</span>
+      </div>
     </div>
   );
 }
