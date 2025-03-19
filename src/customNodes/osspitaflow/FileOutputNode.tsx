25f49d8784aef3460ce000b39551385898094a7a
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-19 15:29:19 +0100

    *add* front / file output node

---
+++ src/customNodes/osspitaflow/FileOutputNode.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Handle, NodeProps, Position, Node } from "@xyflow/react";
+
+export default function FileOutputNode({id, data, isConnectable}: NodeProps<Node<{toWrite : string[]}>>){
+    
+    // should be changed to allow only one connection
+
+    return(
+        <>
+            <Handle
+                type="target"
+                position={Position.Left}
+                onConnect={(params) => console.log('handle onConnect', params)}
+                isConnectable={isConnectable}
+            />
+            <div style={{display:'flex', flexDirection:'column', rowGap:'1rem'}}>
+                <span>Output File</span>
+                <span>Base Name</span>
+                <input className="flex border-1 border-neutral-400 p-2 rounded-sm focus:outline-blue-400" type="text" placeholder="file"/>
+                <span>Extension</span>
+                <input className="flex border-1 border-neutral-400 p-2 rounded-sm focus:outline-blue-400" type="text" placeholder="dat"/>
+                <span>Reset & Overwrite</span>
+                <span>Auto-Increment</span>
+            </div>
+        </>
+    )
+}
