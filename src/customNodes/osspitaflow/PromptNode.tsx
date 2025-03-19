0f9d8b2c92888eb9b02b16c722e7fed9c6d41547
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-19 15:29:36 +0100

    *add* front / prompt node

---
+++ src/customNodes/osspitaflow/PromptNode.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Handle, Position } from "@xyflow/react";
+
+const defaultPromptValue = 'Here should go your prompt...'
+
+export default function PromptNode(){
+
+    function handleTextareaFirstFocus(e : React.FocusEvent<HTMLTextAreaElement>){
+        e.preventDefault()
+        const txtarea = e.currentTarget as HTMLTextAreaElement
+        if(txtarea.value == defaultPromptValue) txtarea.value = ""
+    }
+
+    return(
+        <div className="node p-[10px]">
+            <label>Prompt</label>
+            <textarea onFocus={handleTextareaFirstFocus} defaultValue={defaultPromptValue} className="resize-none border-1 border-neutral-200 p-[5px] rounded-sm bg-neutral-50 text-[10px] w-[300px] focus:outline-none focus:border-blue-400"/>
+            <Handle
+                type="source"
+                position={Position.Right}
+                onConnect={(params) => void 0}
+                isConnectable={true}
+            />
+        </div>
+    )
+}
