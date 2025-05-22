ebcb20c855e650b55bfaceb93aa0ff28e7ebd2ec
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-22 17:23:31 +0200

    *edit* front / new props added to prompt & start node + restyling

--- src/customNodes/osspitaflow/PromptNode.tsx
+++ src/customNodes/osspitaflow/PromptNode.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Handle, Position } from "@xyflow/react";
+import { Handle, NodeProps, Position, Node } from "@xyflow/react";
 
 const defaultPromptValue = 'Here should go your prompt...'
 
-export default function PromptNode(){
+export default function PromptNode({data} : NodeProps<Node<{label : string}>>){
 
     function handleTextareaFirstFocus(e : React.FocusEvent<HTMLTextAreaElement>){
         e.preventDefault()
@@ -13,11 +13,17 @@ export default function PromptNode(){
 
     return(
         <div className="node p-[10px] w-[340px] h-fit gap-y-[0.75rem]">
-            <label>Prompt</label>
+            <label>{data.label ?? 'Prompt'}</label>
             <textarea onFocus={handleTextareaFirstFocus} rows={8} defaultValue={defaultPromptValue} className="resize-none border-1 border-neutral-200 p-[5px] rounded-sm bg-neutral-50 text-[10px] w-full focus:outline-none focus:border-blue-400"/>
             <Handle
                 type="source"
                 position={Position.Right}
+                onConnect={(params) => void 0}
+                isConnectable={true}
+            />
+            <Handle
+                type="target"
+                position={Position.Left}
                 onConnect={(params) => void 0}
                 isConnectable={true}
             />
--- src/customNodes/osspitaflow/StartNode.tsx
+++ src/customNodes/osspitaflow/StartNode.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Handle, NodeProps, Position, Node } from "@xyflow/react";
 
-export default function StartNode({data} : NodeProps<Node<{start : () => void}>>){
+export default function StartNode({data} : NodeProps<Node<{label : string, start : () => void}>>){
 
     return(
         <>
-            <button className="w-[120px]" onClick={data.start}>Start</button>
+            <button className="w-[120px]" onClick={data.start}>{data.label ?? 'Start'}</button>
             <Handle
                 type="source"
                 position={Position.Right}
