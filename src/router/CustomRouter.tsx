249af0499921ab26c8ec85645242a0535b2c0dd1
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-25 18:30:56 +0200

    *edit* front / routing update

--- src/router/CustomRouter.tsx
+++ src/router/CustomRouter.tsx
@@ -4,6 +4,8 @@ import Compute from "../pages/Compute";
 import Spita from "../pages/Spita";
 import ComputeLLM from "../pages/ComputeLLM";
 import ComputeLLM2 from "../pages/ComputeLLM2";
+import ControlledFlowComputation from "../pages/ControlledFlowComputation";
+import TestDifferedFlow from "../pages/TestDifferedFlow";
 
 function CustomRouter() {
 
@@ -11,6 +13,8 @@ function CustomRouter() {
         <BrowserRouter>
                 <Routes>
                   <Route path="/" element={<App />} />
+                  <Route path="/control" element={<ControlledFlowComputation />} />
+                  <Route path="/diff" element={<TestDifferedFlow />} />
                   <Route path="/compute" element={<Compute/>} />
                   <Route path="/llm2" element={<ComputeLLM2/>} />
                   <Route path="/llm" element={<ComputeLLM/>} />
