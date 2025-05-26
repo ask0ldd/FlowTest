ef7dd7d2f7ed0696ce8f03ba6ac280a0ecbaee56
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-26 17:27:51 +0200

    *add* front / startnode

---
+++ src/customNodes/cleanLLMNodes/StartNode.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Handle, NodeProps, Position, Node, useReactFlow } from "@xyflow/react";
+
+export default function StartNode({id, data} : NodeProps<StartNodeType>){
+
+    const { updateNodeData } = useReactFlow();
+
+    function handleStartClick(){
+        updateNodeData(id, {start : true})
+    }
+
+    return(
+        <>
+            <button className="w-[120px]" onClick={handleStartClick}>{data.label ?? 'Start'}</button>
+            <Handle
+                type="source"
+                position={Position.Right}
+                onConnect={(params) => void 0}
+                isConnectable={true}
+            />
+        </>
+    )
+}
+
+export type StartNodeType = Node<{ label: string, start : boolean }, 'start'>;
